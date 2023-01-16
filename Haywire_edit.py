43a91f044a8d2f7afbff402885cfda56d0a32384
intercalaris <ymaviv@gmail.com>
2023-01-16 13:33:22 -0600

    import pyswarms

--- Haywire_edit.py
+++ Haywire_edit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import pyswarms
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
@@ -11,10 +12,8 @@ from matplotlib.colors import ListedColormap
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from deap import base
-from deap import creator
-from deap import tools
-from deap import algorithms
+from deap import base, creator, tools, algorithms
+
 
 # Create a neural network model
 model = Sequential()
@@ -51,24 +50,28 @@ plt.show()
 
 # Swarm Intelligence
 
+# Create a neural network model
+model = Sequential()
+model.add(Dense(50, input_dim=2, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+# Create dataset
+X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_classes=2, n_clusters_per_class=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
 # Define the swarm
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Particle", list, fitness=creator.FitnessMax, speed=list, smin=None, smax=None, best=None)
 
-toolbox = base.Toolbox()
-
 # Define the swarm optimization functions
 def updateParticle(particle, best, phi1, phi2):
     for i in range(len(particle)):
-        particle[i] += phi1 * random.uniform(0, 1) * (best[i] - particle[i]) + phi2 * random.uniform(0, 1) * (global_best[i] - particle[i])
-    return particle
+        particle[i] += phi1 * random.uniform(0, 1) * (best[i] - particle[i]) + phi2 * random.uniform(0, 1) * (best[i] - particle[i])
 
-def evaluate(particle):
-    model.set_weights(particle)
-    _, accuracy = model.evaluate(X_test, to_categorical(y_test), verbose=0)
-    return accuracy,
-
-toolbox.register("particle", tools.initRepeat, creator.Particle, toolbox.attribute, n=len(model.get_weights()))
+toolbox = base.Toolbox()
+toolbox.register("particle", pyswarms.single.GlobalBestPSO.create_particle, size=len(model.get_weights()))
 toolbox.register("population", tools.initRepeat, list, toolbox.particle)
 toolbox.register("update", updateParticle, phi1=2.0, phi2=2.0)
 toolbox.register("evaluate", lambda model, particle: model.evaluate(X_test, to_categorical(y_test), verbose=0)[1])
