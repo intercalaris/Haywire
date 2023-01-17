3d48f463b03ea09235caeba5e9227d44a983dac0
intercalaris <ymaviv@gmail.com>
2023-01-16 19:39:55 -0600

    revert

--- Haywire.py
+++ Haywire.py
@@ -11,10 +11,9 @@ from matplotlib.colors import ListedColormap
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from deap import base
-from deap import creator
-from deap import tools
-from deap import algorithms
+from deap import base, creator, tools, algorithms
+from pyswarms.single import GlobalBestPSO
+from pyswarms.utils.functions import single_obj as fx
 
 # Create a neural network model
 model = Sequential()
@@ -48,47 +47,3 @@ ax = plt.subplot()
 ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, edgecolors='k')
 plt.show()
-
-# Swarm Intelligence
-
-# Create a neural network model
-model = Sequential()
-model.add(Dense(50, input_dim=2, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(2, activation='softmax'))
-model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-
-# Create dataset
-X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_classes=2, n_clusters_per_class=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-
-# Define the swarm
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Particle", list, fitness=creator.FitnessMax, speed=list, smin=None, smax=None, best=None)
-
-# Define the swarm optimization functions
-def updateParticle(particle, best, phi1, phi2):
-    for i in range(len(particle)):
-        particle[i] += phi1 * random.uniform(0, 1) * (best[i] - particle[i]) + phi2 * random.uniform(0, 1) * (best[i] - particle[i])
-
-toolbox = base.Toolbox()
-toolbox.register("particle", tools.initRepeat, creator.Particle, toolbox.attribute, n=len(model.get_weights()))
-toolbox.register("population", tools.initRepeat, list, toolbox.particle)
-toolbox.register("update", updateParticle, phi1=2.0, phi2=2.0)
-toolbox.register("evaluate", lambda model, particle: model.evaluate(X_test, to_categorical(y_test), verbose=0)[1])
-
-# Define the swarm optimization algorithm
-pop = toolbox.population(n=10)
-hof = tools.HallOfFame(1)
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", np.mean)
-stats.register("std", np.std)
-stats.register("min", np.min)
-stats.register("max", np.max)
-
-pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=10, stats=stats, halloffame=hof)
-
-# Print the results
-print("Best individual is: %s\nwith fitness: %s" % (hof[0], hof[0].fitness))
-
-
